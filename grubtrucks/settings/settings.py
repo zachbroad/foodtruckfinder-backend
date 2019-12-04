e25a4c9a7e8149393a08afe2d64eaeed9d72abca
zachbroad <zachbroad@gmail.com>
2019-12-04 12:01:19 -0500

    static

--- grubtrucks/settings/settings.py
+++ grubtrucks/settings/settings.py
@@ -130,7 +130,7 @@ REST_FRAMEWORK = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, '../../db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, '../db.sqlite3'),
     }
 }
 
