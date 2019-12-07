630255ddb3ff5b034730fc016afbcbcdb84ac997
Zach Broad <zachbroad@gmail.com>
2019-12-07 13:17:16 -0500

    settings

--- grubtrucks/settings/production.py
+++ grubtrucks/settings/production.py
@@ -38,11 +38,11 @@ STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY ')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('S3_BUCKET_NAME')
 AWS_S3_REGION_NAME = 'us-east-2'
-AWS_DEFAULT_ACL = None
-AWS_BUCKET_ACL = None
+# AWS_DEFAULT_ACL = None
+# AWS_BUCKET_ACL = None
 
 # ASSETS
 MEDIA_URL = 'https://%s/%s/' % ('grubtrucks-app.s3.amazonaws.com', 'media')
