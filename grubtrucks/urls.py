f85b3c7d1e4a39657511993a2683152dca0cfed3
Zach Broad <zachbroad@gmail.com>
2019-12-06 20:13:03 -0500

    url

--- grubtrucks/urls.py
+++ grubtrucks/urls.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.staticfiles.views import serve
 from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
