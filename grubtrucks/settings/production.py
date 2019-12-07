ed9b14f477b4728a98bac79e573b18c794b0d214
Zach Broad <zachbroad@gmail.com>
2019-12-07 12:56:36 -0500

    aws s3

--- grubtrucks/settings/production.py
+++ grubtrucks/settings/production.py
@@ -17,36 +17,36 @@ DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True
 
 ROOT_URLCONF = 'grubtrucks.urls'
 
-MIDDLEWARE += [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-]
+# MIDDLEWARE += [
+#     'whitenoise.middleware.WhiteNoiseMiddleware',
+# ]
 
-MIDDLEWARE = ['whitenoise.middleware.WhiteNoiseMiddleware'] + MIDDLEWARE
+# MIDDLEWARE = ['whitenoise.middleware.WhiteNoiseMiddleware'] + MIDDLEWARE
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-from whitenoise.storage import CompressedManifestStaticFilesStorage
-
-class WhiteNoiseStaticFilesStorage(CompressedManifestStaticFilesStorage):
-    manifest_strict = False
-
-STATICFILES_STORAGE = 'grubtrucks.settings.production.WhiteNoiseStaticFilesStorage'
-
-#STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# from whitenoise.storage import CompressedManifestStaticFilesStorage
 #
-# AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-# AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY ')
-# AWS_STORAGE_BUCKET_NAME = os.environ.get('S3_BUCKET_NAME')
-# AWS_S3_REGION_NAME = 'us-east-1'
-# AWS_DEFAULT_ACL = None
-# AWS_BUCKET_ACL = None
+# class WhiteNoiseStaticFilesStorage(CompressedManifestStaticFilesStorage):
+#     manifest_strict = False
+#
+# STATICFILES_STORAGE = 'grubtrucks.settings.production.WhiteNoiseStaticFilesStorage'
+
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY ')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('S3_BUCKET_NAME')
+AWS_S3_REGION_NAME = 'us-east-2'
+AWS_DEFAULT_ACL = None
+AWS_BUCKET_ACL = None
 
 # ASSETS
-# MEDIA_URL = 'https://%s/%s/' % ('grubtrucks.s3.amazonaws.com', 'media')
-# STATIC_URL = 'https://%s/%s/' % ('grubtrucks.s3.amazonaws.com', 'static')
+MEDIA_URL = 'https://%s/%s/' % ('grubtrucks-app.s3.amazonaws.com', 'media')
+STATIC_URL = 'https://%s/%s/' % ('grubtrucks-app.s3.amazonaws.com', 'static')
 
 # EMAIL
 # EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
