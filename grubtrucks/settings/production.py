497c230ea955dc0ebbd3eb15ccdcc6ed93b99f30
Zach Broad <zachbroad@gmail.com>
2019-12-07 13:25:07 -0500

    settings

--- grubtrucks/settings/production.py
+++ grubtrucks/settings/production.py
@@ -43,11 +43,8 @@ AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('S3_BUCKET_NAME')
 AWS_S3_REGION_NAME = 'us-east-2'
-AWS_S3_OBJECT_PARAMETERS = {
-    'CacheControl': 'max-age=86400',
-}
-# AWS_DEFAULT_ACL = None
-# AWS_BUCKET_ACL = None
+AWS_DEFAULT_ACL = None
+AWS_BUCKET_ACL = None
 
 # ASSETS
 MEDIA_URL = 'https://%s/%s/' % ('grubtrucks-app.s3.amazonaws.com', 'media')
