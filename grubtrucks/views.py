a0fba17ace9751a07715b574f5502dbbd263b8ed
brandongevat <brandongevat@gmail.com>
2019-12-21 13:58:37 -0500

    testing smtp

--- grubtrucks/views.py
+++ grubtrucks/views.py
@@ -1,17 +1,17 @@
 from django.shortcuts import render
 from django.core.mail import send_mail
-from grubtrucks.settings.production import DEFAULT_TO_EMAIL, EMAIL_HOST_USER, EMAIL_HOST_PASSWORD
+from grubtrucks.settings.production import DEFAULT_TO_EMAIL, EMAIL_HOST_USER, EMAIL_HOST_PASSWORD, DEFAULT_FROM_EMAIL
 
 
 def registration(request, email):
     subject = 'Thank you for registering to ourn site'
     message = ' it means a lot bro'
-    email_from = DEFAULT_TO_EMAIL
+    email_from = DEFAULT_FROM_EMAIL
     host = EMAIL_HOST_USER
     password = EMAIL_HOST_PASSWORD
-    recipient_list = ['brandongevat@gmail.com', email]
+    recipients = ['brandongevat@gmail.com', email]
 
-    send_mail(subject, message, email_from, recipient_list, host, password,)
+    send_mail(subject, message, email_from, recipients, host, password,)
 
 
 def index(request):
