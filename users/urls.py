5d6c7ec93f3126b72fadc76063cc6459fd1613c5
brandongevat <brandongevat@gmail.com>
2019-12-21 15:24:07 -0500

    .

--- users/urls.py
+++ users/urls.py
@@ -5,4 +5,5 @@ from allauth.account import views
 urlpatterns = [
     path('signup/', views.SignupView.as_view(), name='signup'),
     path('login/', views.LoginView.as_view(), name='login'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
 ]
