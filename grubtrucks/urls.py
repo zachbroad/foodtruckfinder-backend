551f6d85b193e41b7157bc1d21365975912a69d1
brandongevat <brandongevat@gmail.com>
2020-01-06 20:43:00 -0500

    password reset

--- grubtrucks/urls.py
+++ grubtrucks/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 
+from allauth.account.views import PasswordResetView
 from rest_framework import routers
 from grubtrucks.views import index
 from trucks.api.views import TruckViewSet
@@ -16,28 +17,29 @@ router.register(r'users', AccountViewSet)
 
 urlpatterns = [
 
-                  # Test site
-                  path(r'', index, name='index'),
-                  path(r'trucks/', include('trucks.urls'), name='trucks-index'),
+        # Test site
+        path(r'', index, name='index'),
+        path(r'trucks/', include('trucks.urls'), name='trucks-index'),
 
-                  # Account
-                  path(r'accounts/', include('allauth.urls', )),
-                  path('users/', include('users.urls')),
-                  path('users/', include('django.contrib.auth.urls')),
+        # Account
+        path(r'accounts/', include('allauth.urls', )),
+        path('users/', include('users.urls')),
+        path('users/', include('django.contrib.auth.urls')),
 
-                  # Admin
-                  path(r'admin/', admin.site.urls),
+        # Admin
+        path(r'admin/', admin.site.urls),
 
-                  # Api
-                  path(r'api/', include((router.urls, '<int:pk>'), namespace='api-trucks')),
+        # Api
+        path(r'api/', include((router.urls, '<int:pk>'), namespace='api-trucks')),
 
-                  # Auth
-                  path(r'api-auth/', include('rest_framework.urls')),
-                  path(r'rest-auth/', include('rest_auth.urls')),
-                  path(r'rest-auth/registration/', include('rest_auth.registration.urls')),
-                  path('login-token/', CustomAuthToken.as_view(), name='login-token'),
-                  path('validate-token/', ValidateToken.as_view(), name='validate-token'),
-                  re_path(r'^static/(?P<path>.*)$', serve,
-                          {'document_root': settings.STATIC_ROOT}),
+        # Auth
+        path(r'api-auth/', include('rest_framework.urls')),
+        path(r'rest-auth/', include('rest_auth.urls')),
+        path(r'rest-auth/registration/', include('rest_auth.registration.urls')),
+        path(r'rest-auth/password/reset/', PasswordResetView.as_view(), name='password-reset'),
+        path('login-token/', CustomAuthToken.as_view(), name='login-token'),
+        path('validate-token/', ValidateToken.as_view(), name='validate-token'),
+        re_path(r'^static/(?P<path>.*)$', serve,
+                {'document_root': settings.STATIC_ROOT}),
 
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
