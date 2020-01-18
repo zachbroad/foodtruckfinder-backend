96b4909b1245b7290456e9940c016c04770cb816
brandongevat <brandongevat@gmail.com>
2020-01-17 21:52:56 -0500

    .

--- trucks/api/views.py
+++ trucks/api/views.py
@@ -1,6 +1,6 @@
-from rest_framework import generics, filters, pagination, viewsets
+from rest_framework import generics, filters, pagination
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.response import Response
+
 from trucks.models import Truck, MenuItem
 from .serializers import TruckSerializer, MenuItemSerializer
 
@@ -10,11 +10,18 @@ class TruckListView(generics.CreateAPIView):  # DetailView CreateView FormView
     serializer_class = TruckSerializer
 
     def get_queryset(self):
-        queryset = Account.objects.all()
+        queryset = Truck.objects.all()
+        title = self.request.query_params.get('title')
+
+        
+        if title is not None:
+            queryset = queryset.filter(title=title)
+
 
         return queryset
 
-
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
 
 
 class TruckDetailView(generics.RetrieveUpdateDestroyAPIView):  # DetailView CreateView FormView
@@ -37,10 +44,8 @@ class TruckViewSet(ModelViewSet):
     serializer_class = TruckSerializer
     queryset = Truck.objects.all()
 
-    filter_fields = ('title')
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('title', 'description', 'tags')
     pagination_class = pagination.LimitOffsetPagination
-
-    def get_queryset(self):
-        return Account.objects.all()
 
 
