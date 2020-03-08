f126ca642b37d13f377b5f0ac0087fa72d0c3402
brandongevat <brandongevat@gmail.com>
2020-03-08 14:26:50 -0400

    Favorite trucks serializer

--- users/api/serializers.py
+++ users/api/serializers.py
@@ -1,9 +1,17 @@
 from rest_framework import serializers
-from users.models import Account, SearchTerm
+from users.models import Account, SearchTerm, FavoriteTruck
+
+
+class FavoriteTruckSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = FavoriteTruck
+        fields = (
+            'truck',
+        )
 
 
 class SearchTermSerializer(serializers.ModelSerializer):
-
     user = serializers.CurrentUserDefault()
 
     class Meta:
@@ -14,8 +22,11 @@ class SearchTermSerializer(serializers.ModelSerializer):
             'created'
         )
 
+
 class AccountSerializer(serializers.ModelSerializer):
     search_history = SearchTermSerializer(many=True, required=False)
+    favorite_trucks = FavoriteTruckSerializer(many=True, required=False)
+
     class Meta:
         model = Account
         fields = (
@@ -26,6 +37,7 @@ class AccountSerializer(serializers.ModelSerializer):
             'first_name',
             'last_name',
             'search_history',
+            'favorite_trucks'
 
         )
         read_only_fields = ['pk']
