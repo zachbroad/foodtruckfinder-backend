b293507e81f78c80a2bf21a87bbf7abd133a3407
Zach Broad <zachbroad@gmail.com>
2020-07-18 20:11:13 -0400

    fix: forgot to add catering files

---
+++ catering/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+from trucks.models import Truck
+
+
+class CaterRequest(models.Model):
+    name = models.CharField(max_length=128)
+    email = models.EmailField()
+    details = models.TextField(max_length=10000)
+    truck = models.ForeignKey(Truck, on_delete=models.DO_NOTHING)
+
+    when = models.DateTimeField()
+    duration = models.FloatField()
+
+    def __str__(self):
+        return 'Cater request by {} for truck {} at {} for {} hours'.format(self.name, self.truck, self.when, self.duration)
