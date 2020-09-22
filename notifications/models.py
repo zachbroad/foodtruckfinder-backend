76b59911c5b0d46a25644680aa6d014011ab0153
Zach Broad <zachbroad@gmail.com>
2020-09-21 22:20:41 -0400

    fix: changed notifications ordering by date

--- notifications/models.py
+++ notifications/models.py
@@ -3,6 +3,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from fcm_django.models import FCMDevice
 
+
 class Notification(models.Model):
     title = models.CharField(max_length=250)
     description = models.TextField(max_length=1000)
@@ -12,13 +13,18 @@ class Notification(models.Model):
     user = models.ForeignKey('users.User', on_delete=models.CASCADE)
     posted_on = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        ordering = ('-posted_on',)
 
     def __str__(self):
         return self.title
-    
+
+
 @receiver(post_save, sender=Notification)
 def notify_on_notification_creation(sender, instance, created, **kwargs):
     if created:
         devices = FCMDevice.objects.all()
-        devices.send_message(data={"click_action": "FLUTTER_NOTIFICATION_CLICK", "id": "1", "status": "done", "priority": "high", "route": instance.route, "webview_route": instance.webview_route},
-                            title=instance.title, body=instance.description, icon="icon_notif")
+        devices.send_message(
+            data={"click_action": "FLUTTER_NOTIFICATION_CLICK", "id": "1", "status": "done", "priority": "high", "route": instance.route,
+                  "webview_route": instance.webview_route},
+            title=instance.title, body=instance.description, icon="icon_notif")
